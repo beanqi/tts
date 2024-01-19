244b77501c8ee476f652e8a4f13dbf127e6e7d11
smark <ynlbq@outlook.com>
2024-01-19 19:14:38 +0800

    Add .epub and scraper dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use epub::doc::EpubDoc;
+use scraper::Html;
+
 fn main() {
-    println!("Hello, world!");
+    let doc = EpubDoc::new("TheEconomist.2024.01.20.trans.epub");
+    assert!(doc.is_ok());
+    let mut doc = doc.unwrap();
+    doc.go_next();
+    doc.go_next();
+    doc.go_next();
+
+    let document = Html::parse_document(&doc.get_current_str().unwrap().0);
 }
