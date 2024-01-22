58d93f5c6c31753254cb7910297253fbe48f310e
smark <ynlbq@outlook.com>
2024-01-22 10:30:02 +0800

    Update TTS voices and add command line argument handling

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,25 @@ use epub_to_speech::tts::edge::Edgetts;
 use epub_to_speech::util::contains_chinese;
 
 fn main() {
-    let mut en_tts = Edgetts::new("en-US-AndrewNeural", "medium", "medium", "medium", "audio-24khz-48kbitrate-mono-mp3");
-    let mut zh_tts = Edgetts::new("zh-CN-YunjianNeural", "medium", "medium", "medium", "audio-24khz-48kbitrate-mono-mp3");
+    let mut en_tts = Edgetts::new("en-US-DavisNeural", "medium", "medium", "loud", "audio-24khz-48kbitrate-mono-mp3");
+    let mut zh_tts = Edgetts::new("zh-CN-YunjianNeural", "medium", "medium", "loud", "audio-24khz-48kbitrate-mono-mp3");
     en_tts.init().unwrap();
     zh_tts.init().unwrap();
-    let file = std::fs::File::open("日本化-Japanization What the World Can Learn from Japans Lost Decades (William Pesek).txt").unwrap();
-    let mut mp3_file = std::fs::File::create("Chepter1.mp3").unwrap();
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <file>", args[0]);
+        return;
+    }
+    let file = std::fs::File::open(&args[1]).unwrap();    
+
+    let mut mp3_file = std::fs::File::create(format!("{}.mp3", args[1])).unwrap();
     let mut reader = std::io::BufReader::new(file);
     let mut line = String::new();
-    let mut count = 0;
     while reader.read_line(&mut line).unwrap() > 0 {
-        print!("generate the lines: {}\n", count);
+        print!("generate the lines: {}\n", line);
         let audio = gen_mp3(&mut en_tts, &mut zh_tts, &line);
         mp3_file.write_all(&audio).unwrap();
-        count += 1;
-        if count > 1013 {
-            break;
-        }
         line.clear();
     }
 }
