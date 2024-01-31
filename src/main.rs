a9badfe77bf72ddd532dda765ab21688a5a7e914
smark <ynlbq@outlook.com>
2024-01-31 08:36:13 +0800

    Refactor text-to-speech functionality and add multi-threading support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Read, Seek, SeekFrom, Write};
+use std::thread;
 
 use epub_to_speech::tts::TTS;
 use epub_to_speech::tts::edge::Edgetts;
@@ -7,19 +8,50 @@ use epub_to_speech::util::contains_chinese;
 use indicatif::ProgressBar;
 
 fn main() {
-    let mut en_tts = Edgetts::new("en-US-AndrewNeural", "medium", "medium", "x-loud", "audio-24khz-96kbitrate-mono-mp3");
-    let mut zh_tts = Edgetts::new("zh-CN-YunjianNeural", "medium", "medium", "x-loud", "audio-24khz-96kbitrate-mono-mp3");
-    en_tts.init().unwrap();
-    zh_tts.init().unwrap();
-
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         println!("Usage: {} <file>", args[0]);
         return;
     }
 
+    let mut handles = Vec::new();
+
+    let file: File = File::open(&args[1]).unwrap();
+    if file.metadata().unwrap().is_dir() {
+        // process all txt files in the directory
+        let files: Vec<String> = std::fs::read_dir(&args[1]).unwrap()
+            .map(|f| f.unwrap().path().to_str().unwrap().to_string())
+            .filter(|f| f.ends_with(".txt"))
+            .collect();
+        for ele in files {
+            handles.push(trans_txt_with_retry(&ele)) ;
+        }
+    } else {
+        handles.push(trans_txt_with_retry(&args[1]));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+fn trans_txt_with_retry(fname: &str) -> thread::JoinHandle<()> {
+    let fname = fname.to_string();
+    thread::spawn(move || {
+        while trans_txt(&fname).is_err() {}
+    })
+}
+
+fn trans_txt(fname: &str) -> anyhow::Result<()> {
+    let mut en_tts = Edgetts::new("en-US-AndrewNeural", "medium", "medium", "x-loud", "audio-24khz-96kbitrate-mono-mp3");
+    let mut zh_tts = Edgetts::new("zh-CN-YunjianNeural", "medium", "medium", "x-loud", "audio-24khz-96kbitrate-mono-mp3");
+    en_tts.init().unwrap();
+    zh_tts.init().unwrap();
+
+
+
     // Open file and read lines into a vector
-    let file = File::open(&args[1]).unwrap();
+    let file: File = File::open(fname).unwrap();
     let reader = BufReader::new(file);
     let mut lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
 
@@ -38,7 +70,7 @@ fn main() {
         .read(true)
         .write(true)
         .create(true)
-        .open(format!("{}.checkpoint", args[1]))
+        .open(format!("{}.checkpoint", fname))
         .unwrap();
 
     // Read checkpoint
@@ -56,19 +88,20 @@ fn main() {
         .read(true)
         .write(true)
         .create(true)
-        .open(format!("{}.mp3", args[1]))
+        .open(format!("{}.mp3", fname))
         .unwrap();
 
     mp3_file.seek(SeekFrom::End(0)).unwrap();
 
     for line in lines {
-        let audio = gen_mp3(&mut en_tts, &mut zh_tts, &line);
+        let audio = gen_mp3(&mut en_tts, &mut zh_tts, &line)?;
         mp3_file.write_all(&audio).unwrap();
         mp3_file.flush().unwrap();
         pb.inc(1);
         write_checkpoint(pb.position() as usize, &mut checkpoint_file);
     }
     pb.finish_with_message("done");
+    Ok(())
 }
 
 fn write_checkpoint(line: usize, checkpoint_file: &mut File) {
@@ -78,7 +111,7 @@ fn write_checkpoint(line: usize, checkpoint_file: &mut File) {
 }
 
 
-fn gen_mp3(en_tts: &mut Edgetts, zh_tts: &mut Edgetts, text: &str) -> Vec<u8> {
+fn gen_mp3(en_tts: &mut Edgetts, zh_tts: &mut Edgetts, text: &str) -> anyhow::Result<Vec<u8>> {
     let audio;
     if contains_chinese(text) {
         audio = zh_tts.gen_audio(text);
@@ -86,8 +119,7 @@ fn gen_mp3(en_tts: &mut Edgetts, zh_tts: &mut Edgetts, text: &str) -> Vec<u8> {
         audio = en_tts.gen_audio(text);
     }
     if audio.is_err() {
-        println!("There some error with the generation,, please try again, the checkpoint is saved.");
-        std::process::exit(1);
+        return Err(anyhow::anyhow!("There some error with the generation"));
     }
-    return audio.unwrap();
+    return Ok(audio.unwrap());
 }
